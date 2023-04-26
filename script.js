988df4e8f3d4d52eb9c415c8f9c2b80309d445ce
Jojokristiadi <100785243+Jojokristiadi@users.noreply.github.com>
2023-04-26 23:06:12 +0700

    finish create animation typing

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Developer", "UI/UX Designer", "Youtuber", "Data Analysist"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+}
+)
